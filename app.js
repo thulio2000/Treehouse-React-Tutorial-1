af07a98cb0fe77b1a119cc2759c123d8117e62e0
thulio2000 <48839223+thulio2000@users.noreply.github.com>
2022-07-12 14:15:29 -0300

    "Render reader variable"

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ const title = React.createElement(
 
 // <h1 id="main-title" title="insert-here">My First React Element!</h1>
 
-ReactDOM.render(
+const desc = React.createElement(
+    'p',
+    null,
+    'I just created a paragraph using react'
+);
+
+const header = React.createElement(
+    'header',
+    null,
     title,
+    desc
+);
+
+ReactDOM.render(
+    header,
     document.querySelector("#root")
 );
 
