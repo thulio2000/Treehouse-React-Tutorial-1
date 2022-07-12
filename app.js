536115ac0bb578aee3e19ea5a5ad3ec325bcde19
thulio2000 <48839223+thulio2000@users.noreply.github.com>
2022-07-12 14:10:00 -0300

    "ReactDOM.render()"

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const title = React.createElement(
     // type , attribute, content
     'h1',
-    null,
-    'My First React Element!'
+    {id: 'main-title', title: 'insert-here'},
+    'My First React Element!'    
+);
+
+// <h1 id="main-title" title="insert-here">My First React Element!</h1>
+
+ReactDOM.render(
+    title,
+    document.querySelector("#root")
 );
 
 console.log(title);
