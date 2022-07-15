593c442ed4a8d50e2077a94379db6dd0a5b8937c
thulio2000 <48839223+thulio2000@users.noreply.github.com>
2022-07-15 08:16:25 -0300

    "Learning Components"

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
-const title = 'My React Element!';
-const desc = 'I created a paragraph using React.';
-const myTitleID = 'main-title';
-const name = 'Lucas';
+const Header = () => {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats" >Players: 1</span>
+        </header>
+    );
+}
 
-const header = (
-    <header>
-        <h1 id={myTitleID}>{name}'s First React Element</h1>
-        <p> { desc } </p>
-    </header>
-);
-//Write 2 or more lines of JSX in parenthesis for readability.
-// { } in JSX are called a JSX expression
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Lucas
+            </span>            
+            <Counter />
+        </div>       
+    );
+}
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">27</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header />
+
+            {/*Players List*/}
+            <Player />
+        </div>
+    );
+}
 
 ReactDOM.render(
-    header,
+    <App />,
     document.getElementById('root')
 );
+
+
+//Elements are the smallest block of react
+//Write 2 or more lines of JSX in parenthesis for readability.
+// { } in JSX are called a JSX expression
+// The curly braces lets JSX know you're writing javascript.
+// day #02 - components, a reusable piece of UI. Created as a JS function or class, I'm not sure what
+// the latter means
+// Apparently, a lot of react syntax is React.createElement(), but converted in a more practical and intuitive way
+// Entire UI composition of functions.
+/*
+day #03 -
+Remember to use className instead of class, just like "getElementByClassName()"
+*/
