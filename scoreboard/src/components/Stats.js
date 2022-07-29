c94e33a940d3fd3b4352b54793f0d0a89a79f0a1
thulio2000 <48839223+thulio2000@users.noreply.github.com>
2022-07-29 18:25:38 -0300

    "PropTypes"

--- scoreboard/src/components/Stats.js
+++ scoreboard/src/components/Stats.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Stats = ({players}) => {
 
@@ -22,5 +23,11 @@ const Stats = ({players}) => {
         </table>
     );
 }
+
+Stats.propTypes = {
+    players: PropTypes.arrayOf(PropTypes.shape({
+        score: PropTypes.number
+    })),
+};
 
 export default Stats;
