3bc345142c65b982f35fc8832c1e36f70f103cea
thulio2000 <48839223+thulio2000@users.noreply.github.com>
2022-07-29 18:33:12 -0300

    "Static and default props"

--- scoreboard/src/components/Player.js
+++ scoreboard/src/components/Player.js
@@ -1,7 +1,18 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import Counter from './Counter';
 
 class Player extends PureComponent {
+
+    static propTypes = {
+      changeScore: PropTypes.func,
+      removePlayer: PropTypes.func,
+      name: PropTypes.string,
+      score: PropTypes.number,
+      id: PropTypes.number,
+      index: PropTypes.number
+    };
+
     render() {
       const { 
         name,
