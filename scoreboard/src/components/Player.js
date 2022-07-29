ed18ac9be387d43dbc8c32cc20e7d0e3f7ecb9c7
thulio2000 <48839223+thulio2000@users.noreply.github.com>
2022-07-29 09:50:16 -0300

    "Purecomponent"

--- scoreboard/src/components/Player.js
+++ scoreboard/src/components/Player.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Counter from './Counter';
 
-const Player = (props) => {
-    return (
-      <div className="player">
-        <span className="player-name">
-          <button className="remove-player" onClick={() => props.removePlayer(props.id)}>✖</button>
-          { props.name }
-        </span>
-  
-        <Counter 
-          score={props.score}
-          index={props.index}
-          changeScore={props.changeScore}
-          />
-      </div>
-    );
+class Player extends PureComponent {
+    render() {
+      const { 
+        name,
+        id,
+        score,
+        index,
+        removePlayer,
+        changeScore
+       } = this.props;
+      return (
+        <div className="player">
+          <span className="player-name">
+            <button className="remove-player" onClick={() => removePlayer(id)}>✖</button>
+            { name }
+          </span>
+    
+          <Counter 
+            score={score}
+            index={index}
+            changeScore={changeScore}
+            />
+        </div>
+      );
+    }    
   }
 
   export default Player;
