0feaa44a4b78bbe8255bbd8d0ede67732eac925b
thulio2000 <48839223+thulio2000@users.noreply.github.com>
2022-08-02 09:10:02 -0300

    "Adding context"

--- scoreboard/src/components/App.js
+++ scoreboard/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Provider } from './Context';
 import Header from './Header';
 import Player from './Player';
 import AddPlayerForm from './AddPlayerForm';
@@ -75,25 +76,27 @@ class App extends Component {
     const highScore = this.getHighScore();
     
     return (
-      <div className="scoreboard">
-        <Header players={this.state.players} />
-  
-        {/* Players list */}
-        {this.state.players.map( (player, index) =>
-          <Player 
-            name={player.name}
-            score={player.score}            
-            id={player.id}
-            changeScore={this.handleScoreChange}
-            key={player.id.toString()} 
-            index={index}
-            removePlayer={this.handleRemovePlayer}           
-            isHighScore={highScore === player.score}
-          />
-        )}
+      <Provider value={this.state.players}>
+        <div className="scoreboard">
+          <Header />
+    
+          {/* Players list */}
+          {this.state.players.map( (player, index) =>
+            <Player 
+              name={player.name}
+              score={player.score}            
+              id={player.id}
+              changeScore={this.handleScoreChange}
+              key={player.id.toString()} 
+              index={index}
+              removePlayer={this.handleRemovePlayer}           
+              isHighScore={highScore === player.score}
+            />
+          )}
 
-        <AddPlayerForm  addPlayer={this.handleAddPlayer}/>
-      </div>
+          <AddPlayerForm  addPlayer={this.handleAddPlayer}/>
+        </div>
+      </Provider>
     );
   }
 }
